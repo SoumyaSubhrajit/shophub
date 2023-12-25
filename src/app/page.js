e681013c962764ad11682cc7da99f9115682d2b1
SoumyaSubhrajit <soumyasubhrajit@gmail.com~>
2023-12-25 23:34:49 +0530

    Adding styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import variable from './variable.module.scss';
-
+import variable from '../../styles/variable.module.scss';
+import '../../styles/pageStyle.scss'
 export default function page() {
   return (
     <div>
-      <h1 style={{color: variable.primaryColor}}>jj</h1>
+      <h1 style={{color: variable.primaryColor}}>Welcome</h1>
+        <h3>some inforamtion</h3>
+        <button>Click Me</button>
+      
     </div>
   )
 }
